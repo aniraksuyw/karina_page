3c3d91cf66e8b24eb02983387795a41977902904
aniraksuyw <48475079+aniraksuyw@users.noreply.github.com>
2019-03-19 17:53:49 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 var photo1 = document.querySelector('.photo1');
 
-photo1.addEventListener("click",function() {
+photo1.addEventListener("click",function()){
     showImage(photo1);
     openLightbox();
 }
